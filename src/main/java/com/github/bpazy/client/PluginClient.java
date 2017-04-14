e880d344a4a72e2894add81782640bb40fa45379
ziyuan <hanziyuan08@outlook.com>
2017-04-14 17:06:30 +0800

    first

---
+++ src/main/java/com/github/bpazy/client/PluginClient.java
@@ -0,0 +1,8 @@
+package com.github.bpazy.client;
+
+/**
+ * Created by Ziyuan
+ * on 2017/4/14
+ */
+public class PluginClient {
+}
---
+++ src/main/java/com/github/bpazy/server/PluginServer.java
@@ -0,0 +1,8 @@
+package com.github.bpazy.server;
+
+/**
+ * Created by Ziyuan
+ * on 2017/4/14
+ */
+public class PluginServer {
+}
